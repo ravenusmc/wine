e927f702c1ebae978d3c0e5c3e315e97f9d00866
ravenusmc <mcuddy77@gmail.com>
2019-01-13 16:51:23 -0500

    added vue to project

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-from project import app
-app.run(debug=True)
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app)
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+
+if __name__ == '__main__':
+    app.run()
