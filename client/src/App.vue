0fc97fcfe627f04ec5dc9c60184b4022706c799e
ravenusmc <mcuddy77@gmail.com>
2019-01-14 19:58:31 -0500

    added navbar and footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Navbar></Navbar>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/support/Navbar';
+import Footer from '@/components/support/Footer';
+
 export default {
   name: 'App',
+  components: {
+    Navbar,
+    Footer,
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ client/src/components/support/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <footer>
+
+      <div class='footer_fix'>
+        <h5>Copyright &copy; 2018 Mike Cuddy Productions<br></h5>
+          <div>
+            <a href="https://www.linkedin.com/in/mcuddy1/"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/mcuddy7"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+            <a href="https://codingsaga.wordpress.com/"><i class="fa fa-pencil fa-3x" aria-hidden="true"></i></a>
+            <a href="https://github.com/ravenusmc"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+          </div>
+      </div>
+
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style>
+.footer_fix {
+  border-top: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 25px;
+  margin-bottom: 50px;
+  padding-top: 50px;
+}
+</style>
