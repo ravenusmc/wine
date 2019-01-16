79a91b24297d559b682c206127fc943dea88864e
ravenusmc <mcuddy77@gmail.com>
2019-01-15 19:53:03 -0500

    added about page and route

--- client/src/components/support/Navbar.vue
+++ client/src/components/support/Navbar.vue
@@ -12,6 +12,9 @@
             <li class="nav-item">
               <router-link class="nav-link" to="/">Home</router-link>
             </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
           </ul>
         </div>
       </nav>
