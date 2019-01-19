96a5ec5c5f6ff9e6546b55eba01e2af5a8943481
ravenusmc <mcuddy77@gmail.com>
2019-01-18 19:57:39 -0500

    deleted old work

--- project/__init__.py
+++
@@ -1,8 +0,0 @@
-#importing outside libraries
-from flask import Flask, session, jsonify, redirect, url_for, escape, render_template, request, flash 
-
-app = Flask(__name__)
-
-from project.site.routes import mod
-
-app.register_blueprint(site.routes.mod)
