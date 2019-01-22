4387b6c6948f46ad7fa6dcd29223c16c4bef03cd
ravenusmc <mcuddy77@gmail.com>
2019-01-21 20:16:04 -0500

    minor changes

--- client/src/pages/Fact.vue
+++ client/src/pages/Fact.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+//Get wine based on region and type. Allow a person to select a wine and see how the
+//average score is across regions 
 export default {
   name: 'Fact',
 };
