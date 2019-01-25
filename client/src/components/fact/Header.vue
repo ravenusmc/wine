fe9d3ee04a82bbce5aea144d7a3e90d615de734d
ravenusmc <mcuddy77@gmail.com>
2019-01-24 20:02:46 -0500

    added background image for header

--- client/src/components/fact/Header.vue
+++ client/src/components/fact/Header.vue
@@ -10,7 +10,15 @@
 <script>
 export default {
   name: 'Header',
-}
+};
 </script>
+
 <style scoped>
+header {
+  height: 500px;
+  background-image: url('../../../src/assets/images/graph1.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 </style>
