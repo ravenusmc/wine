9d4dfa5f7cff84831e87f3fc08da1845385213de
ravenusmc <mcuddy77@gmail.com>
2019-01-25 20:01:59 -0500

    added in basic information component

---
+++ client/src/components/fact/BasicInformation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Basic</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicInformation',
+};
+</script>
+
+<style scoped>
+</style>
--- client/src/pages/Fact.vue
+++ client/src/pages/Fact.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header/>
+    <BasicInformation/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/fact/Header';
+import BasicInformation from '@/components/fact/BasicInformation';
 
 export default {
   name: 'Fact',
   components: {
     Header,
+    BasicInformation,
   },
 };
 </script>
