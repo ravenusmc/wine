477046f07bbfeb3554be53ec6d56b1036b2bbe7e
ravenusmc <mcuddy77@gmail.com>
2019-01-26 11:19:41 -0500

    adding cards for basic facts page

--- client/src/components/fact/BasicInformation.vue
+++ client/src/components/fact/BasicInformation.vue
@@ -1,6 +1,51 @@
 <template>
   <div>
-    <h1>Basic</h1>
+
+    <div class="md-layout md-gutter md-alignment-center">
+
+      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+         <md-ripple>
+           <md-card-header>
+             <div class="md-title">Basic Information</div>
+           </md-card-header>
+
+           <md-card-content>
+             Country with highest scoring wine: 
+           </md-card-content>
+
+           <md-card-actions>
+             <md-button>Action</md-button>
+             <md-button>Action</md-button>
+           </md-card-actions>
+         </md-ripple>
+       </md-card>
+      </div>
+
+      <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <md-card md-with-hover>
+         <md-ripple>
+           <md-card-header>
+             <div class="md-title">Card with hover effect</div>
+             <div class="md-subhead">It also have a ripple</div>
+           </md-card-header>
+
+           <md-card-content>
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+           </md-card-content>
+
+           <md-card-actions>
+             <md-button>Action</md-button>
+             <md-button>Action</md-button>
+           </md-card-actions>
+         </md-ripple>
+       </md-card>
+      </div>
+
+    </div>
+
+
   </div>
 </template>
 
