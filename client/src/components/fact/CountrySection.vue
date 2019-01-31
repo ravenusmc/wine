d3354dfdb75a41e90a694e4dfaf1e601f856a199
ravenusmc <mcuddy77@gmail.com>
2019-01-30 20:08:18 -0500

    added code to start getting countries for user drop down

--- client/src/components/fact/CountrySection.vue
+++ client/src/components/fact/CountrySection.vue
@@ -2,9 +2,20 @@
   <div>
 
     <div class='input_area'>
+      <h3>Please Select a Country</h3>
       <input v-model="country" placeholder="Please Enter a Country">
        <button @click='submitCountry'>Submit</button>
     </div>
+
+    <!-- BELOW FROM: https://github.com/ravenusmc/examining_suicide/blob/master/client/src/components/facts/Calls/By_Country.vue -->
+    <h3 class='font'>Suicides by Country</h3>
+<form @submit="onSubmit">
+  <select v-model="country" name="country">
+    <option v-for="country in countries" :value="country">{{country}}</option>
+  </select>
+  <button type="submit" variant="primary">Submit</button>
+  <p class='font'>The number of suicides in {{ country }} is: {{ msg }}</p>
+</form>
 
   </div>
 </template>
@@ -15,6 +26,7 @@ export default {
   data(){
     return {
       country: '',
+      countries: [],
     }
   },
   methods: {
