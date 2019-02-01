811d6cf14909dc4271521a6abb588c10217812a4
ravenusmc <mcuddy77@gmail.com>
2019-01-31 19:09:43 -0500

    added new file

---
+++ server/server.py
@@ -0,0 +1,10 @@
+#This file will provide support to me in this project. A class used in this
+#file will not be used in the project but provide data that I'll need to use.
+
+#import libraries for use in the file
+import numpy as np
+import pandas as pd
+
+class Support():
+
+    
