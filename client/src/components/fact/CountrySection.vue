332b9f1d4b3bb6485b5d7c6e2ffa2c592bf69b5d
ravenusmc <mcuddy77@gmail.com>
2019-02-02 11:15:13 -0500

    got form working for country

--- client/src/components/fact/CountrySection.vue
+++ client/src/components/fact/CountrySection.vue
@@ -2,20 +2,13 @@
   <div>
 
     <div class='input_area'>
-      <h3>Please Select a Country</h3>
-      <input v-model="country" placeholder="Please Enter a Country">
-       <button @click='submitCountry'>Submit</button>
+      <form @submit="submitCountry">
+        <select v-model="country" name="country">
+          <option v-for="country in countries" :value="country">{{country}}</option>
+        </select>
+        <button type="submit" variant="primary">Submit</button>
+      </form>
     </div>
-
-    <!-- BELOW FROM: https://github.com/ravenusmc/examining_suicide/blob/master/client/src/components/facts/Calls/By_Country.vue -->
-    <h3 class='font'>Suicides by Country</h3>
-<form @submit="onSubmit">
-  <select v-model="country" name="country">
-    <option v-for="country in countries" :value="country">{{country}}</option>
-  </select>
-  <button type="submit" variant="primary">Submit</button>
-  <p class='font'>The number of suicides in {{ country }} is: {{ msg }}</p>
-</form>
 
   </div>
 </template>
@@ -27,13 +20,14 @@ export default {
     return {
       country: '',
       countries: ['US', 'Spain', 'France', 'Italy', 'New Zealand', 'Bulgaria', 'Argentina',
-                  'Australia', 'Portugal', 'Israel', 'South Africa', 'Greece', 'Chile', 'Morocco',
-                  'Romania', 'Germany', 'Canada', 'Moldova', 'Hungary', 'Austria', 'Croatia',
-                  'Slovenia', 'India', 'Turkey', 'Macedonia', 'Lebanon', 'Serbia', 'Uruguay',
-                  'Switzerland', 'Albania', 'Bosnia and Herzegovina', 'Brazil', 'Cyprus',
-                  'Lithuania', 'Japan', 'China', 'South Korea', 'Ukraine', 'England' 'Mexico',
-                  'Georgia', 'Montenegro', 'Luxembourg', 'Slovakia', 'Czech Republic', 'Egypt',
-                  'Tunisia', 'US-France'],
+          'Australia', 'Portugal', 'Israel', 'South Africa', 'Greece', 'Chile', 'Morocco',
+          'Romania', 'Germany', 'Canada', 'Moldova', 'Hungary', 'Austria', 'Croatia',
+          'Slovenia', 'India', 'Turkey', 'Macedonia', 'Lebanon', 'Serbia', 'Uruguay',
+          'Switzerland', 'Albania', 'Bosnia and Herzegovina', 'Brazil', 'Cyprus',
+          'Lithuania', 'Japan', 'China', 'South Korea', 'Ukraine', 'England', 'Mexico',
+          'Georgia', 'Montenegro', 'Luxembourg', 'Slovakia', 'Czech Republic', 'Egypt',
+          'Tunisia', 'US-France'],
+
     }
   },
   methods: {
--- client/src/store/modules/fact.js
+++ client/src/store/modules/fact.js
@@ -17,67 +17,67 @@ import axios from 'axios';
     standardDeviationPoints: state => state.standardDeviationPoints,
   };
 
-  const actions = {
+const actions = {
     getHighestRatedWine: ({ commit }) => {
       const path = 'http://localhost:5000/highestRatedWine';
       axios.get(path)
-        .then((res) => {
-          commit('setHighestRatedWine', res.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getAverageWineScore: ({ commit }) => {
-      const path = 'http://localhost:5000/getBasicData';
-      axios.get(path)
-        .then((res) => {
-          commit('setAverageWineScore', res.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getLowestScore: ({ commit }) => {
-      const path = 'http://localhost:5000/getlowestscore';
-      axios.get(path)
-        .then((res) => {
-          commit('setLowestScore', res.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getStandardDeviation: ({ commit }) => {
-      const path = 'http://localhost:5000/getStandardDeviation';
-      axios.get(path)
-        .then((res) => {
-          commit('setStandardDeviationPoints', res.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-  };
-
-  const mutations = {
-    setHighestRatedWine(state, data) {
-      state.highestRatedWines = data;
-    },
-    setAverageWineScore(state, data) {
-      state.averageWineScore = data;
-    },
-    setLowestScore(state, data) {
-      state.lowestWineScore = data;
-    },
-    setStandardDeviationPoints(state, data) {
-      state.standardDeviationPoints = data;
+      .then((res) => {
+        commit('setHighestRatedWine', res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  getAverageWineScore: ({ commit }) => {
+    const path = 'http://localhost:5000/getBasicData';
+    axios.get(path)
+    .then((res) => {
+        commit('setAverageWineScore', res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  getLowestScore: ({ commit }) => {
+    const path = 'http://localhost:5000/getlowestscore';
+    axios.get(path)
+      .then((res) => {
+        commit('setLowestScore', res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  getStandardDeviation: ({ commit }) => {
+    const path = 'http://localhost:5000/getStandardDeviation';
+    axios.get(path)
+      .then((res) => {
+        commit('setStandardDeviationPoints', res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     },
   };
 
-  export default {
-    state,
-    getters,
-    actions,
-    mutations
-  }
+const mutations = {
+  setHighestRatedWine(state, data) {
+    state.highestRatedWines = data;
+  },
+  setAverageWineScore(state, data) {
+    state.averageWineScore = data;
+  },
+  setLowestScore(state, data) {
+    state.lowestWineScore = data;
+  },
+  setStandardDeviationPoints(state, data) {
+    state.standardDeviationPoints = data;
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
