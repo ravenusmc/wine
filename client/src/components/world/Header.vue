83fd7d527e8d65fb1da374e349b0c9e9e61b16f4
ravenusmc <mcuddy77@gmail.com>
2019-02-06 19:50:49 -0500

    added to header area on world map page

--- client/src/components/world/Header.vue
+++ client/src/components/world/Header.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
+
     <header>
       <h1>World Map</h1>
     </header>
+
+    <div class='input_area'>
+        <select @click='hide' v-model="variety" name="country">
+          <option v-for="country in countries" :value="country">{{country}}</option>
+        </select>
+        <md-button @click="submitCountry" type="submit" variant="primary" class="md-raised md-primary">Primary</md-button>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Header',
+  data(){
+    return {
+      
+    }
+  }
 }
 </script>
+
 <style scoped>
+header {
+  text-align: center;
+  margin-top: 50px;
+}
 </style>
