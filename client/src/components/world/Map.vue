11dc3460bf72ec09512bfb301f8a212c365445e0
ravenusmc <mcuddy77@gmail.com>
2019-02-09 13:17:49 -0500

    wrote code to pass a prop

---
+++ client/src/components/world/Map.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>MAP</h1>
+    {{ variety }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  props: {
+    variety: {
+      type: String,
+      required: false
+    },
+  }
+
+
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/pages/World.vue
+++ client/src/pages/World.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <World @wine-variety="wineVariety"/>
+    <Map :variety="variety" />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 import World from '@/components/world/Header';
+import Map from '@/components/world/Map';
 
 export default {
   name: 'WorldMap',
   components: {
     World,
+    Map,
   },
   data() {
     return {
